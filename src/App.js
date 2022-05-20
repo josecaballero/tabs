d30e13ceea2c52a3b2fadbb2694b5f0c56331ac4
Jose <jlcab8@gmail.com>
2022-05-20 13:17:09 +0200

    added filtering jobs by tab

--- src/App.js
+++ src/App.js
@@ -36,6 +36,15 @@ function App() {
         <div className="underline"></div>
       </div>
       <div className="jobs-center">
+        <div className="btn-container">
+          {jobs.map((job, index) => {
+            return (
+              <button onClick={() => setValue(index)} key={job.id}>
+                {job.company}
+              </button>
+            );
+          })}
+        </div>
         <article className="job-info">
           <h3>{title}</h3>
           <h4>{company}</h4>
